a9fc0077fe07b429b612ab3b917f59de0c577bc0
Mike Jorgensen <mikeljorgensen@gmail.com>
2017-12-05 08:44:59 -0700

    finished question 9

--- app-9/src/App.js
+++ app-9/src/App.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import {Link} from 'react-router-dom';
+import Router from './Router'
+
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+
+    }
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <nav>
+          <ul className='Navs'>
+            <Link to='/'>Home</Link>
+            <Link to='/signup'>Signup</Link>
+            <Link to='/details'>Details</Link>
+          </ul>
+        </nav>
+        <br/>
+        {Router}
       </div>
     );
+  }
+  switchFocus(focus){
+    this.setState({
+      focus: focus
+    })
   }
 }
 
